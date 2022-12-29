c9aa9da5299d68e845edbf1771123744dddc9d5b
Byron Marohn <Combustible@live.com>
2022-12-28 18:09:30 -0800

    More WIP
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- conda_reset.sh
+++ conda_reset.sh
@@ -1,11 +1,17 @@
+# This is not really a script so much as a sense of notes. 
 conda deactivate
 conda env remove -n ipex
 conda create -n ipex python=3.9 -y
 conda activate ipex
-pip install ~/Downloads/*.whl
+
+pip install ~/Downloads/intel_extension_for_pytorch-1.10.200+gpu-cp39-cp39-linux_x86_64.whl
+pip install ~/Downloads/torch-1.10.0a0+git3d5f2d4-cp39-cp39-linux_x86_64.whl
 pip install diffusers ftfy transformers Pillow
 pip install accelerate
+
+# TODO: Where did this come from?
 source /opt/intel/oneapi/mkl/2022.2.1//env/vars.sh
+
 pip install clip
 pip install fastapi
 pip install gradio
@@ -26,6 +32,7 @@ pip install piexif
 pip install fonts font-roboto
 pip install addict filterpy
 pip install lmdb numba numpy yapf future gdown tb-nightly 
+pip install blendmodes
 
 python -m pip install torchvision==0.11.0+cpu --no-deps -f https://download.pytorch.org/whl/torch_stable.html
 pip install lpips --no-deps
@@ -39,3 +46,6 @@ pip install open-clip-torch --no-deps
 
 # This seems to cause a segfault even though it's a dependency of open-clip-torch
 #pip install sentencepiece
+
+To run: python3 webui.py --no-half-vae --no-half
+
